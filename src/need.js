9c681b7260e497d999dfc993d5a84c656c6e6a8d
Martin Dederer <md@spielmeister.com>
2012-05-25 15:18:44 +0200

    need.js: added static require
     * Only modules that have already been defined can be required.

--- src/need.js
+++ src/need.js
@@ -2,96 +2,75 @@ var need = {
 	modules: {}
 }
 
-
 var resolveDependencies = function( moduleName, config ) {
-	if( moduleName === undefined ||
-		moduleName === "" ) {
-
-		throw "No module name was provided."
-	}
+	if( !moduleName ) throw 'No module name was provided.'
 
 
 	var module = need.modules[ moduleName ]
 
-	if( module === undefined ||
-		module.definition === undefined ) {
-
-		throw "Unable to find module definition for module '" + moduleName + "'."
-	}
+	if( !module ) throw 'Unable to find module definition for module \'' + moduleName + '\'.'
 
 
-	var dependencies = module.definition[ 0 ]
-	var callback     = module.definition[ 1 ]
-
-	var args = []
+	var dependencies = module.dependencies,
+		args = []
 
 	for( var i = 0; i < dependencies.length; i++ ) {
-		var name = dependencies[ i ]
+		var name = dependencies[ i ],
+			dependencyModule = need.modules[ name ]
 
-		if( need.modules[ name ] === undefined ) {
-			throw 'Could not find module definition for dependency "' + name + '" of module "' + moduleName + '" . Is it included and registered via define?'
+		if( !dependencyModule ) {
+			throw 'Could not find module definition for dependency \'' + name + '\' of module \'' + moduleName + '\' . Is it included and registered via define?'
 		}
 
-		if( need.modules[ name ].instance === undefined ) {
-			need.modules[ name ].instance = resolveDependencies( dependencies[ i ] )
+		if( !dependencyModule.instance ) {
+			dependencyModule.instance = resolveDependencies( dependencies[ i ] )
 		}
 
 		args.push(
-			need.modules[ name ].instance
+			dependencyModule.instance
 		)
 	}
 
-	if( config ) {
-		args.push( config )
-	}
+	if( config ) args.push( config )
 
-	return callback.apply( null, args )
+	return module.body.apply( null, args )
 }
 
 
-var define = function( name, dependencies, callback ) {
+var define = function( moduleName, dependencies, moduleBody ) {
 	if( arguments.length < 2 ||
 		arguments.length > 3 ) {
 
-		throw "Definition is invalid."
+		throw 'Module definition is invalid.'
 	}
 
 
 	if( arguments.length === 2 ) {
-		// No dependencies were provided. Thus arguments looks like this [ name, constructor ].
+		// No dependencies were provided. Thus the arguments look like this [ name, constructor ].
 
-		callback = dependencies
+		moduleBody = dependencies
 		dependencies = []
 	}
 
-	var module = {
-		definition: [ dependencies, callback ]
+	need.modules[ moduleName ] = {
+		dependencies : dependencies,
+		body : moduleBody
 	}
-
-	need.modules[ name ] = module
 }
 
 
-var require = function( dependencies, callback ) {
-	if( dependencies === undefined ||
-		callback === undefined ) {
-
-		throw "The provided arguments do not match."
-	}
+var require = function( moduleName ) {
+	if( !moduleName ) throw 'No module name provided.'
 
 
-	var args = []
+	var module = need.modules[ moduleName ]
 
-	for( var i = 0; i < dependencies.length; i++ ) {
-		args.push(
-			resolveDependencies( dependencies[ i ] )
-		)
-	}
+	if( !module ) throw 'Could not resolve module name \'' + moduleName + '\' to module instance.'
 
 
-	callback.apply( null, args )
+	return module.instance
 }
 
-enterMain = function( mainModuleName, args ) {
-	resolveDependencies( mainModuleName, args )
+enterMain = function( moduleName, args ) {
+	resolveDependencies( moduleName, args )
 }
