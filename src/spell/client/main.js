c5611af4d90bdc363b856a6f0fa19b5eebe5ce46
Martin Dederer <md@spielmeister.com>
2012-05-30 18:25:53 +0200

    Cleaning up

--- src/spell/client/main.js
+++ src/spell/client/main.js
@@ -49,6 +49,30 @@ define(
 		'use strict'
 
 
+		var loadBlueprints = function( blueprintManager, runtimeModule ) {
+			_.each(
+				runtimeModule.componentBlueprints,
+				function( componentBlueprint ) {
+					blueprintManager.add( componentBlueprint )
+				}
+			)
+
+			_.each(
+				runtimeModule.entityBlueprints,
+				function( entityBlueprint ) {
+					blueprintManager.add( entityBlueprint )
+				}
+			)
+
+			_.each(
+				runtimeModule.systemBlueprints,
+				function( systemBlueprint ) {
+					blueprintManager.add( systemBlueprint )
+				}
+			)
+		}
+
+
 		if( parameters.verbose ) {
 			Logger.setLogLevel( Logger.LOG_LEVEL_DEBUG )
 		}
@@ -56,87 +80,50 @@ define(
 		Logger.debug( 'client started' )
 
 
-		var eventManager         = new EventManager()
-		var configurationManager = new ConfigurationManager( eventManager, parameters )
-
-		var renderingContext = PlatformKit.RenderingFactory.createContext2d(
-			eventManager,
-			configurationManager.id,
-			1024,
-			768,
-			configurationManager.renderingBackEnd
-		)
-
-		var soundManager         = PlatformKit.createSoundManager()
-		var inputManager         = new InputManager( configurationManager )
-		var resourceLoader       = new ResourceLoader( runtimeModule.name, soundManager, renderingContext, eventManager, configurationManager.resourceServer )
-		var statisticsManager    = new StatisticsManager()
+		var globals              = {},
+			eventManager         = new EventManager(),
+			configurationManager = new ConfigurationManager( eventManager, parameters ),
+			renderingContext     = PlatformKit.RenderingFactory.createContext2d(
+				eventManager,
+				configurationManager.id,
+				1024,
+				768,
+				configurationManager.renderingBackEnd
+			),
+			soundManager         = PlatformKit.createSoundManager(),
+			inputManager         = new InputManager( configurationManager ),
+			resourceLoader       = new ResourceLoader( runtimeModule.name, soundManager, renderingContext, eventManager, configurationManager.resourceServer ),
+			statisticsManager    = new StatisticsManager(),
+			blueprintManager     = new BlueprintManager(),
+			mainLoop             = createMainLoop( eventManager, statisticsManager ),
+			zoneManager          = new ZoneManager( globals, eventManager, blueprintManager, mainLoop )
 
 		statisticsManager.init()
-
-		var globals = {
-			configurationManager : configurationManager,
-			eventManager         : eventManager,
-			inputManager         : inputManager,
-			inputEvents          : inputManager.getInputEvents(),
-			renderingContext     : renderingContext,
-			resourceLoader       : resourceLoader,
-			resources            : resourceLoader.getResources(),
-			statisticsManager    : statisticsManager,
-			soundManager         : soundManager
-		}
-
-		var configurationManager = globals.configurationManager
-		var eventManager         = globals.eventManager
-		var statisticsManager    = globals.statisticsManager
-
-//		PlatformKit.registerOnScreenResize( _.bind( onScreenResized, onScreenResized, eventManager ) )
-
-		var renderingContext       = globals.renderingContext
-		var renderingContextConfig = renderingContext.getConfiguration()
-
-		Logger.debug( 'created rendering context: type=' + renderingContextConfig.type + '; size=' + renderingContextConfig.width + 'x' + renderingContextConfig.height )
-
-
-		var blueprintManager = new BlueprintManager()
-
-		_.each(
-			runtimeModule.componentBlueprints,
-			function( componentBlueprint ) {
-				blueprintManager.add( componentBlueprint )
-			}
-		)
-
-		_.each(
-			runtimeModule.entityBlueprints,
-			function( entityBlueprint ) {
-				blueprintManager.add( entityBlueprint )
-			}
-		)
-
-		_.each(
-			runtimeModule.systemBlueprints,
-			function( systemBlueprint ) {
-				blueprintManager.add( systemBlueprint )
-			}
-		)
-
-
-		var mainLoop    = createMainLoop( eventManager, statisticsManager )
-		var zoneManager = new ZoneManager( globals, eventManager, blueprintManager, mainLoop )
-
-
-		var entityManager = new EntityManager( blueprintManager )
 
 		_.extend(
 			globals,
 			{
 				configurationManager : configurationManager,
-				entityManager        : entityManager,
 				eventManager         : eventManager,
+				entityManager        : new EntityManager( blueprintManager ),
+				inputManager         : inputManager,
+				inputEvents          : inputManager.getInputEvents(),
+				renderingContext     : renderingContext,
+				resourceLoader       : resourceLoader,
+				resources            : resourceLoader.getResources(),
+				statisticsManager    : statisticsManager,
+				soundManager         : soundManager,
 				zoneManager          : zoneManager
 			}
 		)
+
+//		PlatformKit.registerOnScreenResize( _.bind( onScreenResized, onScreenResized, eventManager ) )
+
+		var renderingContextConfig = renderingContext.getConfiguration()
+		Logger.debug( 'created rendering context: type=' + renderingContextConfig.type + '; size=' + renderingContextConfig.width + 'x' + renderingContextConfig.height )
+
+
+		loadBlueprints( blueprintManager, runtimeModule )
 
 
 		var zoneConfig = _.find(
