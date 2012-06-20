ee04d98efe7c8a7f1023b735e300ea1136e3156b
Martin Dederer <md@spielmeister.com>
2012-06-20 15:19:48 +0200

    Fixed bug that falsely excluded certain modules from trace result

---
+++ src/spell/shared/util/createEntityEach.js
@@ -0,0 +1,24 @@
+define(
+	'spell/shared/util/create',
+	function() {
+		'use strict'
+
+
+		var create = function( constructor, args ) {
+			if ( constructor.prototype === undefined ) {
+				throw create.NO_CONSTRUCTOR_ERROR + constructor
+			}
+
+			var object = {}
+			object.prototype = constructor.prototype
+			var returnedObject = constructor.apply( object, args )
+			return returnedObject || object
+		}
+
+
+		create.NO_CONSTRUCTOR_ERROR = 'The first argument for create must be a constructor. You passed in '
+
+
+		return create
+	}
+)
