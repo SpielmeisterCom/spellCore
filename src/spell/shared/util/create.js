34460834e125e0acffc357d80f9a7b03c964711c
Martin Dederer <md@spielmeister.com>
2012-06-22 16:41:38 +0200

    Revised "create" to support property injection
     * Now "create" supports the injection of properties into the scope of the instance prior to the execution of the constructor function. That means that the injected properties are available via the "this pointer" in the constructor function.

--- src/spell/shared/util/create.js
+++ src/spell/shared/util/create.js
@@ -1,24 +1,36 @@
 define(
 	'spell/shared/util/create',
-	function() {
+	[
+		'spell/shared/util/platform/underscore'
+	],
+	function(
+		_
+	) {
 		'use strict'
 
 
-		var create = function( constructor, args ) {
+		var NO_CONSTRUCTOR_ERROR = 'The first argument for create must be a constructor. You passed in '
+
+
+		/**
+		 * public
+		 */
+
+		// TODO: Fix the issue that instances which where created with "create" do not support method look-up along the prototype chain.
+		return function( constructor, constructorArguments, propertiesToInject ) {
 			if ( constructor.prototype === undefined ) {
-				throw create.NO_CONSTRUCTOR_ERROR + constructor
+				throw NO_CONSTRUCTOR_ERROR + constructor
 			}
 
 			var object = {}
+
+			if( !!propertiesToInject && _.isObject( propertiesToInject ) ) {
+				_.extend( object, propertiesToInject )
+			}
+
 			object.prototype = constructor.prototype
-			var returnedObject = constructor.apply( object, args )
+			var returnedObject = constructor.apply( object, constructorArguments )
 			return returnedObject || object
 		}
-
-
-		create.NO_CONSTRUCTOR_ERROR = 'The first argument for create must be a constructor. You passed in '
-
-
-		return create
 	}
 )
