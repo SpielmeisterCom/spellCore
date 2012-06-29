f44a415a6e8e6c9976a0aa91016df944d348dacc
Julian Haupt <jh@spielmeister.com>
2012-06-29 20:17:21 +0200

    Added components stub file for documentation generation

---
+++ docs/components.js
@@ -0,0 +1,20 @@
+/**
+  * @class spell.component.2d.transform
+  * A duck, not just a stupid bird.
+  */
+spell.component.component.2d = {
+	/**
+	 * @cfg {Array} Translation
+	 */
+	translation: [0,0],
+	
+	/**
+	 * @cfg {Array} Scale
+	 */
+	scale: [1,1],
+
+	/**
+	 * @cfg {Float} Rotation
+	 */
+	rotation: 0.0
+};
