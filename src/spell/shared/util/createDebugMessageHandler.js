54d0ea4205ee6dc166f365a0609afad046f934a4
Julian Haupt <jh@spielmeister.com>
2012-09-02 18:17:27 +0200

    Handle updateComponent messages

--- src/spell/shared/util/createDebugMessageHandler.js
+++ src/spell/shared/util/createDebugMessageHandler.js
@@ -12,6 +12,17 @@ define(
 				'spelled.debug.executeRuntimeModule' : function( payload ) {
 					spell.runtimeModule = payload
 					startEngine( payload )
+				},
+				'spelled.debug.updateComponent' : function( payload ) {
+					//TODO: check if the scene is correct
+
+					var component = spell.entityManager.getComponent( payload.entityId, payload.componentId );
+					if ( component ) {
+						spell.logger.debug( "Setting " + payload.componentId + ":" + payload.key + " = " + payload.value + " in entity " + payload.entityId)
+						component[payload.key] = payload.value
+					} else {
+						spell.logger.debug( "Could not find component " + payload.componentId + " in entity " + payload.entityId )
+					}
 				}
 			}
 
