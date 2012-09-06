d92c6fe7992559c3a988b2df3a848cfb6628ab1b
Martin Dederer <md@spielmeister.com>
2012-09-06 14:44:03 +0200

    Added audible signal to watch script

--- watch.sh
+++ watch.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+
+SOUND_FILE="/home/martin/Musik/218.wav"
+
 inotifywait -m -r src -e modify | while read event; do
 	make
 	echo done...
+	if [ -e "$SOUND_FILE" ]
+	then
+		play -q $SOUND_FILE
+	fi
 done
