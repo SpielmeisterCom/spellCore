e4ba8a7cd6206424d21a2b75e9c63e9263a47a94
Martin Dederer <md@spielmeister.com>
2012-10-24 12:14:31 +0200

    Cleaned up OrderedMap

--- src/spell/shared/util/OrderedMap.js
+++ src/spell/shared/util/OrderedMap.js
@@ -10,77 +10,70 @@ define(
 
 
 		var OrderedMap = function() {
-		    this.sortedKeys = []
-		    this.map = {}
-		};
+		    this.keys = []
+		    this.values = []
+		}
 
 		OrderedMap.prototype = {
 		    add : function( key, value ) {
-		        var map = this.map
+				var keys   = this.keys,
+					values = this.values,
+					index  = keys.indexOf( key )
 
-		        if( !map[ key ] ) {
-		            this.sortedKeys.push( key )
-		        }
+				if( index === -1 ) {
+					keys.push( key )
+					values.push( value )
 
-				map[ key ] = value
+				} else {
+					values[ index ] = value
+				}
 
 				return this
 		    },
 		    insert : function( key, value, index ) {
-		        this.sortedKeys.splice( index, 0, key )
-		        this.map[ key ] = value
+		        this.keys.splice( index, 0, key )
+		        this.values.splice( index, 0, value )
 
 				return this
 		    },
 			getByIndex : function( index ) {
-				var sortedKeys = this.sortedKeys
-
-    			var key = sortedKeys[ index ]
-
-				if( !key ) return
-
-				return this.map[ key ]
+				return this.values[ index ]
 			},
 			getByKey : function( key ) {
-				return this.map[ key ]
+				return this.getByIndex( this.keys.indexOf( key ) )
 			},
 		    removeByIndex : function( index ) {
-		        var sortedKeys = this.sortedKeys
-
-		        var key = sortedKeys[ index ]
-
-		        if( !key ) return
-
-		        arrayRemove( sortedKeys, index )
-		        delete this.map[ key ]
+		        arrayRemove( this.keys, index )
+				arrayRemove( this.values, index )
 
 				return this
 		    },
 		    removeByKey : function( key ) {
-		        this.removeByIndex( this.sortedKeys.indexOf( key ) )
+		        this.removeByIndex( this.keys.indexOf( key ) )
 
 				return this
 		    },
 		    each : function( iter ) {
-		        var map        = this.map,
-		            sortedKeys = this.sortedKeys
+		        var keys   = this.keys,
+		            values = this.values
 
-		        for( var i = 0, numSortedKeys = sortedKeys.length; i < numSortedKeys; i++ ) {
-		            var key = sortedKeys[ i ]
+		        for( var i = 0, numKeys = keys.length; i < numKeys; i++ ) {
+		            var key   = keys[ i ],
+						value = values[ i ]
 
-		            iter( map[ key ], key )
+		            iter( value, key )
 		        }
 
 				return this
 		    },
 			clear : function() {
-				this.map = {}
-				this.sortedKeys.length = 0
+				this.keys.length = 0
+				this.values.length = 0
 
 				return this
 			},
 		    size : function() {
-		        return this.sortedKeys.length
+		        return this.keys.length
 		    }
 		}
 
