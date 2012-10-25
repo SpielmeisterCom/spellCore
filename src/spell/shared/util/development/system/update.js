ef6286f98dbd9da7b70a37751ade9ccb92a10cad
Martin Dederer <md@spielmeister.com>
2012-10-25 13:43:20 +0200

    An exception is triggered when spell.debug.system.update message is malformed

--- src/spell/shared/util/development/system/update.js
+++ src/spell/shared/util/development/system/update.js
@@ -12,7 +12,12 @@ define(
 		return function( spell, payload ) {
 			var definition = payload.definition
 
+			if( !definition.namespace || !definition.name ) {
+				throw 'Error: System definition is missing namespace and or name attribute.'
+			}
+
 			spell.templateManager.add( definition, true )
+
 			spell.sceneManager.restartSystem(
 				createId( definition.namespace, definition.name ),
 				payload.executionGroupId,
