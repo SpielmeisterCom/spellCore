e8803aa14b0ddaec50c404eeb46e329009db10a9
Julian Haupt <julian.haupt@spielmeister.com>
2012-11-17 02:10:07 +0100

    wrote a super minimalistic test frameworks which integrates well in our structure. all available test frameworks sucked in one way or another

---
+++ test/spell/math/mat3.js
@@ -0,0 +1,24 @@
+define(
+	"test/spell/math/mat3",
+	[
+		"spell/math/mat3"
+	],
+	function( mat3 ) {
+		"use strict";
+
+		var mat3Test = function( describe, it ) {
+
+			describe("mat3Test", function() {
+				it("should compose and decompose for a combined transform, scale, rotation correctly", function() {
+
+				})
+			})
+
+
+		}
+
+		return mat3Test
+	}
+
+
+)
--- test/spellCoreTestSuite.js
+++ test/spellCoreTestSuite.js
@@ -1,11 +1,68 @@
 define(
 	"test/spellCoreTestSuite",
-	[],
+	[
+		"test/spell/math/mat3"
+	],
 	function() {
 		"use strict";
 
-		return "hello world"
+		var testSuites          = [ ],
+			tests               = [ ],
+			currentTestSuite    = "",
+			numberOfTotalTests  = 0,
+			numberOfFailedTests = 0
+
+		var describe = function( testSuiteName, testSuiteCallback ) {
+			testSuites.push({
+				name: 		testSuiteName,
+				callback:   testSuiteCallback
+			})
+		}
+
+		var it = function( testDescription, testCallback ) {
+			tests.push({
+				testSuite:          currentTestSuite,
+				testDescription:    testDescription,
+				callback:           testCallback
+			})
+		}
+
+		//load tests from all included modules
+		for( var i=0; i < arguments.length; i++ ) {
+			arguments[ i ].call( undefined, describe, it )
+		}
+
+		//evaluate tests from all testsuites
+		for(var i=0, length=testSuites.length; i < length; i++ ) {
+			var testSuite = testSuites[ i ]
+
+			currentTestSuite = testSuite.name
+			testSuite.callback.call()
+		}
+
+		numberOfTotalTests = tests.length
+
+		//run all tests now
+		for(var i=0; i < numberOfTotalTests; i++ ) {
+			var test = tests[ i ],
+				logLine = "[" + (i+1) + "/" + numberOfTotalTests + "] <" + test.testSuite + "> " + test.testDescription + " "
+
+			try {
+				test.callback.call()
+				logLine += "PASSES"
+
+			} catch( e ) {
+				logLine += "FAILED"
+				numberOfFailedTests++
+			}
+
+			console.log( logLine )
+		}
+
+
+		console.log ( " ")
+
+		console.log ( numberOfFailedTests + " tests failed!" )
+
 	}
-
-
 )
