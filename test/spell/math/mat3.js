43eed08ba78771a593832145d3d5f910ee40eb08
Julian Haupt <julian.haupt@spielmeister.com>
2012-11-17 16:32:10 +0100

    WIP mat3 testcases

--- test/spell/math/mat3.js
+++ test/spell/math/mat3.js
@@ -153,7 +153,18 @@ define(
 					matrixB = mat3.create(),
 					testMatrix = mat3.create([1,2,3,4,5,6,7,8,9]),
 					vector = vec2.create([2.5, -4.5]),
-					checkMatrix = [ 2.5, -9,  3,  10,  -22.5,  6,  7,  8,  9]
+					x = vector[0],
+					y = vector[1],
+					checkMatrix = [
+						testMatrix[0] * x,
+						testMatrix[1] * y,
+						testMatrix[2],
+						testMatrix[3] * x,
+						testMatrix[4] * y,
+						testMatrix[5],
+						testMatrix[6] * x,
+						testMatrix[7] * y,
+						testMatrix[8] ]
 
 				mat3.set( testMatrix, matrix)
 				mat3.scale( matrix, vector, matrix )
@@ -178,61 +189,131 @@ define(
 
 			it("should be able to extract the scale from a matrix that is only scaled", function() {
 				var matrix = mat3.create(),
-					scale  = vec2.create([2.5, -6.4])
+					scaleA = vec2.create([2.5, -6.4]),
+					scaleB = vec2.create([-4.2, 5]),
+					decomposedScale
 
 				mat3.identity( matrix )
-				mat3.scale( matrix, scale )
-				var decomposedScale = mat3.getScale( matrix )
+				mat3.scale( matrix, scaleA )
+				decomposedScale = mat3.getScale( matrix )
 
-				if (!vec2.equal( decomposedScale, scale )) {
+				if (!vec2.equal( decomposedScale, scaleA )) {
 					throw 'decomposed scale ' + vec2.str( decomposedScale ) + ' does not match the scale ' + vec2.str( scale ) + ' matrix was ' + mat3.str( matrix )
+				}
+
+				mat3.identity( matrix )
+				mat3.scale( matrix, scaleB )
+				decomposedScale = mat3.getScale( matrix )
+
+				if (!vec2.equal( decomposedScale, scaleB  )) {
+					throw 'decomposed scale ' + vec2.str( decomposedScale ) + ' does not match the scale ' + vec2.str( scaleB ) + ' matrix was ' + mat3.str( matrix )
 				}
 			})
 
 			it("should rotate a identity 3x3-matrix correctly", function() {
-				var matrix              = mat3.create(),
-					testValues          = [ Math.PI, -Math.PI, Math.PI/2, -Math.PI/2, Math.PI/4, -Math.PI/4, Math.PI+Math.PI/2, -Math.PI-Math.PI/2 ]
+				var matrix              = mat3.create()
 
-				for (var i=0; i<testValues.length; i++) {
-					var phi = testValues[ i ]
-
+				for ( var phi = -2 * Math.PI; phi <= 2*Math.PI; phi += Math.PI/8 ) {
 					//rotate left
-					var rotationMatrix = mat3.create([ Math.cos(-phi), Math.sin(-phi), 0, -Math.sin(-phi), Math.cos(-phi), 0, 0, 0, 1])
-					var testMatrix = mat3.create()
-					mat3.identity( testMatrix )
-					mat3.multiply( testMatrix, rotationMatrix, testMatrix )
+					var rotationMatrix = mat3.create([
+						Math.cos(phi),
+						-1 * Math.sin(phi),
+						0,
+
+						Math.sin(phi),
+						Math.cos(phi),
+						0,
+
+						0,
+						0,
+						1])
 
 					mat3.identity( matrix )
 					mat3.rotate( matrix, phi )
 
-					if (!mat3.equal(matrix, testMatrix)) {
-						throw 'did not rotate matrix correctly by ' + phi + ' expected ' + mat3.str( testMatrix ) + ' got ' + mat3.str( matrix )
+					if (!mat3.equal(matrix, rotationMatrix)) {
+						throw 'did not rotate matrix correctly by ' + phi + ' expected ' + mat3.str( rotationMatrix ) + ' got ' + mat3.str( matrix )
 					}
 				}
 			})
 
 			it("should be able to extract the rotation from a 3x3-matrix correctly", function() {
-				var matrix              = mat3.create(),
-					testValues          = [ Math.PI, -Math.PI, Math.PI/2, -Math.PI/2, Math.PI/4, -Math.PI/4, Math.PI+Math.PI/2, -Math.PI-Math.PI/2 ]
+				var matrix              = mat3.create()
 
-				for (var i=0; i<testValues.length; i++) {
-					var phi = testValues[ i ]
+				var errors = []
 
-					//rotate left
+				for( var phi = -2 * Math.PI; phi <= 2 * Math.PI; phi += Math.PI / 8 ) {
 					mat3.identity( matrix )
 					mat3.rotate( matrix, phi )
 
 					var decomposedRotation = mat3.getRotation( matrix )
 
-					if (phi !== decomposedRotation) {
-						throw 'could not extract rotation from 3x3 matrix expected ' + phi + ' got ' + decomposedRotation
+					if(phi !== decomposedRotation && Math.abs(phi - decomposedRotation) != Math.PI*2) {
+						errors.push( 'could not extract rotation from 3x3 matrix expected ' + phi + ' got ' + decomposedRotation + ' difference ' + (phi-decomposedRotation))
 					}
+				}
+
+				if( errors.length > 0 ) {
+					throw errors.join("\n")
 				}
 			})
 
-			it("should compose and decompose for a combined transform, scale, rotation correctly", function() {
-				var matrix = mat3.create()
+			it("should be able to scale, rotate and transform a matrix", function() {
+				var matrix      = mat3.create(),
+					scale       = vec2.create( [ -3, 5 ] ),
+					transform   = vec2.create( [ 10, -100 ] ),
+					phi         = 2,
+					checkMatrix = mat3.create([
+						Math.cos(phi) * scale[ 0 ],
+						-1*Math.sin(phi) * scale[ 1 ],
+						0,
+
+						Math.sin(phi) * scale[ 0 ],
+						Math.cos(phi) * scale[ 1 ],
+						0,
+
+						transform[ 0 ],
+						transform[ 1 ],
+						1])
+
+
 				mat3.identity( matrix )
+				mat3.scale( matrix, scale )
+				mat3.rotate( matrix, phi )
+				mat3.translate( matrix, transform )
+
+				if(!mat3.equal( matrix, checkMatrix )) {
+					throw 'expected ' + mat3.str( checkMatrix ) + ' got ' + mat3.str( matrix )
+				}
+
+			})
+
+			it("should compose and decompose for a combined translate, scale, rotation correctly", function() {
+				var matrix      = mat3.create(),
+					scale       = vec2.create( [ -3, 5 ] ),
+					translation   = vec2.create( [ 10, -100 ] ),
+					rotation    = -2
+
+				mat3.identity( matrix )
+				//mat3.translate( matrix, translation )
+				mat3.rotate( matrix, rotation )
+				mat3.scale( matrix, scale )
+
+				var checkScale          = mat3.getScale( matrix ),
+					checkRotation       = mat3.getRotation( matrix ),
+					checkTranslation    = mat3.getTranslation( matrix),
+					checkSkew           = mat3.getSkew( matrix )
+
+
+				if(!vec2.equal( checkScale, scale )) {
+					throw 'expected scale: ' + vec2.str( scale ) +  ' got ' + vec2.str( checkScale ) + ' matrix is ' + mat3.str( matrix ) + ' skew is ' + vec2.str( checkSkew )
+
+				} /*else if (!vec2.equal( checkTranslation, translation )) {
+					throw 'expected translation: ' + vec2.str( translation ) +  ' got ' + vec2.str( checkTranslation )
+
+				} */else if ( rotation != checkRotation || Math.abs( checkRotation - rotation ) !== 2*Math.PI ) {
+					throw 'expected rotation: ' + rotation + ' got ' + checkRotation
+				}
 			})
 		})
 
