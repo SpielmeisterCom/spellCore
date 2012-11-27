1baaf4997475c6a90f99a2ecbaa102c99bb226a7
Julian Haupt <julian.haupt@spielmeister.com>
2012-11-27 20:26:57 +0100

    current WIP tilemap editor

--- library/spell/script/editor/tilemapEditor.js
+++ library/spell/script/editor/tilemapEditor.js
@@ -11,33 +11,54 @@ define(
 			STATE_DRAW_TILE     = 2
 
 		var showTilemapSelector = function( cursorWorldPosition, tilemapAsset ) {
+			var entityManager               = this.spell.entityManager,
+				spriteSheetAssetId          = tilemapAsset.asset.spriteSheet.assetId,
+				frameDimensions             = tilemapAsset.asset.spriteSheet.frameDimensions,
+				numFrames                   = tilemapAsset.asset.spriteSheet.numFrames,
+				tilemapSelectionEntityId    = null
 
-			var entityManager = this.spell.entityManager,
-				tilemapSelectionEntityId = null
+			//present a nice quadratic selection menu for the tiles
+			var framesPerRow = Math.floor( Math.sqrt(numFrames) ) + 1
 
-			tilemapSelectionEntityId = entityManager.createEntity({
+			this.tilemapSelection = entityManager.createEntity({
 				'config': {
 					'spell.component.2d.transform': {
 						'translation': cursorWorldPosition
 					},
 					'spell.component.2d.graphics.spriteSheetAppearance': {
-						'assetId': tilemapAsset.asset.spriteSheet.assetId
+						'assetId': spriteSheetAssetId
+					},
+					'spell.component.2d.graphics.geometry.quad': {
+						dimensions: [ framesPerRow * frameDimensions[ 0 ], framesPerRow * frameDimensions[ 1 ] ]
+					},
+					'spell.component.visualObject': {
+						'layer': 99999998
+					}
+				}
+			})
+/*
+			var tilemapSelectionCursor = entityManager.createEntity({
+				'config': {
+					'spell.component.2d.transform': {
+						'translation': cursorWorldPosition
+					},
+					'spell.component.2d.graphics.spriteSheetAppearance': {
+						'assetId': spriteSheetAssetId
 					},
 					'spell.component.visualObject': {
 						'layer': 99999999
 					}
 				}
 			})
-
-	/*		var tilemapSelectionCursor = entityManager.createEntity({
-
-			})
 */
 		}
 
 		var tilemapEditor = function(spell, editorSystem) {
-			this.state  = STATE_INACTIVE
-			this.spell  = spell
+			this.state              = STATE_INACTIVE
+			this.spell              = spell
+
+			this.tilemapSelection       = null
+			this.tilemapSelectionCursor = null
 		}
 
 		tilemapEditor.prototype = {
