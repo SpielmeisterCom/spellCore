ad21283b0441e38361216b490d34b3a3a7a22afe
Julian Haupt <julian.haupt@spielmeister.com>
2012-12-07 20:48:50 +0100

    added support to run spelljs without an container object in the dom

--- src/spell/shared/util/platform/private/graphics/createCanvasNode.js
+++ src/spell/shared/util/platform/private/graphics/createCanvasNode.js
@@ -2,18 +2,22 @@ define(
 	'spell/shared/util/platform/private/graphics/createCanvasNode',
 	function() {
 		return function( id, width, height ) {
-			var container = document.getElementById( id )
-
-			if( !container ) throw 'Could not find a container with the id ' + id + ' in the DOM tree.'
-
-
 			var canvas = document.createElement( 'canvas' )
 				canvas.id     = id + '-screen'
 				canvas.width  = width
 				canvas.height = height
 				canvas.className += 'spell-canvas'
 
-			container.appendChild( canvas )
+
+			if( id ) {
+				var container = document.getElementById( id )
+				if( !container ) throw 'Could not find a container with the id ' + id + ' in the DOM tree.'
+
+				container.appendChild( canvas )
+
+			} else {
+				document.body.appendChild( canvas )
+			}
 
 			return canvas
 		}
