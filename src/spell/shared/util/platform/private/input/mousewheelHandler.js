a7eb245955412c7e02ba65c87bd764e7f0689e57
Julian Haupt <julian.haupt@spielmeister.com>
2013-01-13 23:37:08 +0100

    Refactored MouseWheel Handler

---
+++ src/spell/shared/util/platform/private/input/mousewheelHandler.js
@@ -0,0 +1,63 @@
+/**
+ * WheelEvents
+ *
+ * interface WheelEvent : MouseEvent {
+ *      // DeltaModeCode
+ *      const unsigned long              DOM_DELTA_PIXEL = 0x00;
+ *      const unsigned long              DOM_DELTA_LINE  = 0x01;
+ *      const unsigned long              DOM_DELTA_PAGE  = 0x02;
+ *
+ *      readonly attribute double         deltaX;
+ *      readonly attribute double         deltaY;
+ *      readonly attribute double         deltaZ;
+ *      readonly attribute unsigned long deltaMode;
+ *  };
+ */
+define(
+	'spell/shared/util/platform/private/input/mousewheelHandler',
+	[
+		'spell/functions'
+	],
+	function( _ ) {
+		"use strict";
+
+		var nativeHandler = null
+
+		var nativeHandlerImpl = function( callback, event ) {
+			event.preventDefault()
+
+			var delta     = event.wheelDelta ? event.wheelDelta : event.detail * -1,
+				direction = delta > 0 ? 1 : -1
+
+			callback( {
+				type : 'mousewheel',
+				direction : direction
+			} )
+
+
+		}
+
+		var registerListener = function( el, callback ) {
+			nativeHandler = _.bind( nativeHandlerImpl, this, callback )
+
+			el.addEventListener( 'mousewheel', nativeHandler, true )
+			el.addEventListener( 'DOMMouseScroll', nativeHandler, true )
+		}
+
+		var removeListener = function( el ) {
+			if( nativeKeyHandler !== null ) {
+				el.removeEventLister( 'mousewheel', nativeHandler )
+				el.removeEventLister( 'DOMMouseScroll', nativeHandler )
+			}
+
+			nativeKeyHandler = null
+		}
+
+		return {
+			registerListener: registerListener,
+			removeListener: removeListener
+		}
+
+
+	}
+)
