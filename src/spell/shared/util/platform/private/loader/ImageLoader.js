bd25ac302fd3a1b434bf7d2bb3f9acfe40e3f546
Martin Dederer <md@spielmeister.com>
2013-01-24 16:48:55 +0100

    Fixed potential memory leak

--- src/spell/shared/util/platform/private/loader/ImageLoader.js
+++ src/spell/shared/util/platform/private/loader/ImageLoader.js
@@ -17,15 +17,23 @@ define(
 		 * private
 		 */
 
+		var free = function( image ) {
+			image.onload = null
+			image.onreadystatechange = null
+			image.onerror = null
+		}
+
 		var onLoad = function( image ) {
 			if( this.loaded === true ) return
 
 			this.loaded = true
 
+			free( image )
 			this.onLoadCallback( image )
 		}
 
-		var onError = function( event ) {
+		var onError = function( image, event ) {
+			free( image )
 			this.onErrorCallback( event )
 		}
 
@@ -41,19 +49,20 @@ define(
 		 */
 
 		var ImageLoader = function( resourcePath, resourceName, onLoadCallback, onErrorCallback, onTimedOutCallback ) {
-			this.resourcePath       = resourcePath
-			this.resourceName       = resourceName
-			this.onLoadCallback     = onLoadCallback
-			this.onErrorCallback    = onErrorCallback
-			this.loaded             = false
+			this.resourcePath    = resourcePath
+			this.resourceName    = resourceName
+			this.onLoadCallback  = onLoadCallback
+			this.onErrorCallback = onErrorCallback
+			this.loaded          = false
 		}
 
 		ImageLoader.prototype = {
-			start: function() {
+			start : function() {
 				var image = new Image()
+
 				image.onload             = _.bind( onLoad, this, image )
 				image.onreadystatechange = _.bind( onReadyStateChange, this, image )
-				image.onerror            = _.bind( onError, this )
+				image.onerror            = _.bind( onError, this, image )
 				image.src                = this.resourcePath + '/' + this.resourceName
 			}
 		}
