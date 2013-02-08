1e1f298d7e7c5a19f33c22961841cf040eba3a62
Martin Dederer <martin.dederer@spielmeister.com>
2013-02-08 17:01:27 +0100

    Changed platformDetails

--- src/spell/shared/util/platform/private/platformDetails.js
+++ src/spell/shared/util/platform/private/platformDetails.js
@@ -8,39 +8,30 @@ define(
 	) {
 		'use strict'
 
-		//For html5 only. Parse userAgent etc.
-		var getBrowser = function() {
-			var N= navigator.appName, ua= navigator.userAgent, tem;
-			var M= ua.match(/(opera|chrome|safari|firefox|msie)\/?\s*(\.?\d+(\.\d+)*)/i);
-			if(M && (tem= ua.match(/version\/([\.\d]+)/i))!= null) M[2]= tem[1];
-			M= M? [M[1], M[2]]: [N, navigator.appVersion, '-?'];
-
-			return M;
-		}
 
 		return {
-			platformId : 'html5',
 			hasPlentyRAM : function() { return true },
 			hasTouchSupport : function() {
 				return supportedPointerApi.hasWebkitTouchApi() ||
 					supportedPointerApi.hasPointerTouchApi()
+			},
+			getColorDepth: function() {
+				return screen.colorDepth
+			},
+			getOS : function() {
+				return navigator.platform
+			},
+			getPlatformAdapter : function() {
+				return 'html5'
+			},
+			getPlatform : function() {
+				return navigator.userAgent
 			},
 			getScreenHeight: function() {
 				return screen.height
 			},
 			getScreenWidth: function() {
 				return screen.width
-			},
-			getColorDepth: function() {
-				return screen.colorDepth
-			},
-			getRuntime: function() {
-				var browser = getBrowser()
-
-				return {
-					version: browser[1],
-					name   : browser[0]
-				}
 			}
 		}
 	}
