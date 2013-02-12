85685ed1ca9770e0ec1f2c25546d6fb8df7fe3d5
Martin Dederer <md@spielmeister.com>
2013-02-12 18:33:27 +0100

    Cleaning up

--- src/spell/shared/util/platform/private/graphics/Viewporter.js
+++ src/spell/shared/util/platform/private/graphics/Viewporter.js
@@ -20,51 +20,12 @@ define(
 			var viewporter = {}
 
 			viewporter.renderViewport = function() {
-				var createViewportMetaTag = function( initialScale ) {
-					var meta = document.createElement( 'meta' )
-					meta.name    = 'viewport'
-					meta.content = 'width=device-width; initial-scale=' + initialScale + '; maximum-scale=' + initialScale + '; user-scalable=0;'
-
-					return meta
-				}
-
-				var getOffset = function( element ) {
-					var box = element.getBoundingClientRect()
-
-					var body    = document.body
-					var docElem = document.documentElement
-
-					var scrollTop  = window.pageYOffset || docElem.scrollTop || body.scrollTop
-					var scrollLeft = window.pageXOffset || docElem.scrollLeft || body.scrollLeft
-
-					var clientTop  = docElem.clientTop || body.clientTop || 0
-					var clientLeft = docElem.clientLeft || body.clientLeft || 0
-
-					var top  = box.top + scrollTop - clientTop
-					var left = box.left + scrollLeft - clientLeft
-
-					return [ Math.round( left ), Math.round( top ) ]
-				}
-
 				var publishScreenResizedEvent = function() {
 					eventManager.publish( Events.AVAILABLE_SCREEN_SIZE_CHANGED, [ getAvailableScreenSize( id ) ] )
 				}
 
-
-				if( navigator.userAgent.match( /iPhone/i ) ||
-					navigator.userAgent.match( /iPod/i ) ) {
-
-					document.getElementsByTagName( 'head' )[ 0 ].appendChild( createViewportMetaTag( '0.5' ) )
-
-				} else if( navigator.userAgent.match( /iPad/i ) ) {
-
-					document.getElementsByTagName( 'head' )[ 0 ].appendChild( createViewportMetaTag( '1.0' ) )
-				}
-
-
 				// initialize viewporter object
 				viewporter = {
-
 					// options
 					forceDetection: false,
 
@@ -80,7 +41,6 @@ define(
 					ready: function(callback) {
 						window.addEventListener('viewportready', callback, false)
 					}
-
 				}
 
 				// if we are on Desktop, no need to go further
@@ -92,32 +52,31 @@ define(
 
 				// create private constructor with prototype..just looks cooler
 				var _Viewporter = function() {
-
 					var that = this
 
 					this.IS_ANDROID = /Android/.test(navigator.userAgent)
 
 					var _onReady = function() {
-
 						// scroll the shit away and fix the viewport!
 						that.prepareVisualViewport()
 
 						// listen for orientation change
 						var cachedOrientation = window.orientation;
-						window.addEventListener('orientationchange', function() {
-							if(window.orientation != cachedOrientation) {
-								that.prepareVisualViewport()
-								cachedOrientation = window.orientation
-							}
-						}, false)
-
+						window.addEventListener(
+							'orientationchange',
+							function() {
+								if( window.orientation != cachedOrientation ) {
+									that.prepareVisualViewport()
+									cachedOrientation = window.orientation
+								}
+							},
+							false
+						)
 					}
-
 
 					// listen for document ready if not already loaded
 					// then try to prepare the visual viewport and start firing custom events
 					_onReady()
-
 				}
 
 				_Viewporter.prototype = {
@@ -167,35 +126,32 @@ define(
 
 						// start the checker loop
 						var iterations = this.IS_ANDROID && !deviceProfile ? 20 : 5 // if we're on Android and don't know the device, brute force hard
-						var check = window.setInterval(function() {
+						var check = window.setInterval(
+							function() {
+								// retry scrolling
+								window.scrollTo(0, that.IS_ANDROID ? 1 : 0) // Android needs to scroll by at least 1px
 
-							// retry scrolling
-							window.scrollTo(0, that.IS_ANDROID ? 1 : 0) // Android needs to scroll by at least 1px
-
-							if(
-								that.IS_ANDROID
+								if( that.IS_ANDROID
 									? (deviceProfile ? window.innerHeight === deviceProfile[orientation] : --iterations < 0) // Android: either match against a device profile, or brute force
-									: (window.innerHeight > startHeight || --iterations < 0) // iOS is comparably easy!
-							) {
-								clearInterval(check)
+									: (window.innerHeight > startHeight || --iterations < 0) /* iOS is comparably easy! */ ) {
 
-								// set minimum height of content to new window height
-								document.documentElement.style.minHeight = window.innerHeight + 'px'
+									clearInterval(check)
 
-								if( id && document.getElementById(id) ) {
-									// set the right height for the body wrapper to allow bottom positioned elements
-									document.getElementById(id).style.position = 'relative'
-									document.getElementById(id).style.height = window.innerHeight + 'px'
+									// set minimum height of content to new window height
+									document.documentElement.style.minHeight = window.innerHeight + 'px'
 
-									// fire events, get ready
-									that.postProcess( )
+									if( id && document.getElementById(id) ) {
+										// set the right height for the body wrapper to allow bottom positioned elements
+										document.getElementById(id).style.position = 'relative'
+										document.getElementById(id).style.height = window.innerHeight + 'px'
+
+										// fire events, get ready
+										that.postProcess( )
+									}
 								}
-
-
-							}
-
-						}, 10)
-
+							},
+							10
+						)
 					},
 
 					triggerWindowEvent: function(name) {
@@ -203,12 +159,10 @@ define(
 						event.initEvent(name, false, false)
 						window.dispatchEvent(event)
 					}
-
 				};
 
 				// initialize
 				new _Viewporter()
-
 			}
 
 			viewporter.profiles = {
