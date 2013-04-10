b796ef3e56944233156828dc3140a43c840e6003
Ioannis Kostomanolakis <ik@spielmeister.com>
2013-04-10 12:08:37 +0200

    SPELLJS-1093 - Repair export and init projects - Fixed spellCorePath in spellcli-deploy-begin.js

--- spellcli-deploy-begin.js
+++ spellcli-deploy-begin.js
@@ -1,7 +1,7 @@
 var path          = require( 'path' ),
-	requirejs	= require( 'requirejs' ),
-	define		= requirejs.define,
-	spellCorePath	= path.resolve( process.execPath )
+	requirejs	  = require( 'requirejs' ),
+	define		  = requirejs.define,
+	spellCorePath = path.resolve( path.dirname( process.execPath ) )
 
 requirejs.config( {
 	nodeRequire: require
