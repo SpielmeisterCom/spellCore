63a683f0c075d27227f357388ccb08d2f27927a0
Julian Haupt <jh@spielmeister.com>
2013-04-30 01:55:13 +0200

    added buildNumber and buildTimeStamp to configuration

--- src/spell/shared/Configuration.js
+++ src/spell/shared/Configuration.js
@@ -8,7 +8,9 @@ define(
 
 
 		var Configuration = {
-			version : '0.0.2',
+		        version                : '9.9.9',
+        		buildNumber            : '99999',
+        		buildTimeStamp         : '2099-01-01T01:00:00.000+01:00',
 		}
 
 		return Configuration 
