52e53f37de0a4a04e9800db67383acba455932d4
Julian Haupt <julian.haupt@spielmeister.com>
2013-04-30 23:39:11 +0200

    Changed version to 0.8.5

--- src/spell/shared/Configuration.js
+++ src/spell/shared/Configuration.js
@@ -8,7 +8,7 @@ define(
 
 
 		var Configuration = {
-		        version                : '9.9.9',
+		        version                : '0.8.5',
         		buildNumber            : '99999',
         		buildTimeStamp         : '2099-01-01T01:00:00.000+01:00',
 		}
