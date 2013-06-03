20ab393e9f2f20dc90c73c98a8706798b365b397
Martin Dederer <md@spielmeister.com>
2013-06-03 17:01:42 +0200

    Add support for interstitials when run in game closure runtime

--- src/spell/shared/util/platform/private/advertisement.js
+++ src/spell/shared/util/platform/private/advertisement.js
@@ -1,32 +1,40 @@
 define(
 	'spell/shared/util/platform/private/advertisement',
 	[
+		'spell/shared/util/platform/private/isHtml5CocoonJS',
 		'spell/shared/util/platform/private/isHtml5Ejecta',
-		'spell/shared/util/platform/private/isHtml5CocoonJS'
+		'spell/shared/util/platform/private/isHtml5GameClosure'
 	],
 	function(
+		isHtml5CocoonJS,
 		isHtml5Ejecta,
-		isHtml5CocoonJS
+		isHtml5GameClosure
 	) {
 		'use strict'
 
 
 		return {
 			loadInterstitial : function() {
-				if( isHtml5Ejecta ) {
+				if( isHtml5CocoonJS ) {
+					CocoonJS.Ad.preloadFullScreen()
+
+				} else if( isHtml5Ejecta ) {
 					ejecta.loadInterstitial()
 
-				} else if( isHtml5CocoonJS ) {
-					CocoonJS.Ad.preloadFullScreen()
+				} else if( isHtml5GameClosure ) {
+					NATIVE.advertisement.loadInterstitial()
 				}
 			},
 			showInterstitial : function() {
-				if( isHtml5Ejecta ) {
-					ejecta.showInterstitial()
-
-				} else if( isHtml5CocoonJS ) {
+				if( isHtml5CocoonJS ) {
 					CocoonJS.Ad.setBannerLayout( CocoonJS.Ad.BannerLayout.TOP_CENTER )
 					CocoonJS.Ad.showFullScreen()
+
+				} else if( isHtml5Ejecta ) {
+					ejecta.showInterstitial()
+
+				} else if( isHtml5GameClosure ) {
+					NATIVE.advertisement.showInterstitial()
 				}
 			}
 		}
