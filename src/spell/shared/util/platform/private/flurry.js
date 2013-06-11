87b1436cc01af0c110faf50c78e314f9cf00a494
Martin Dederer <md@spielmeister.com>
2013-06-11 15:10:05 +0200

    Added support for flurry on android target to platform adapter

--- src/spell/shared/util/platform/private/flurry.js
+++ src/spell/shared/util/platform/private/flurry.js
@@ -1,10 +1,12 @@
 define(
 	'spell/shared/util/platform/private/flurry',
 	[
-		'spell/shared/util/platform/private/isHtml5Ejecta'
+		'spell/shared/util/platform/private/isHtml5Ejecta',
+		'spell/shared/util/platform/private/isHtml5GameClosure'
 	],
 	function(
-		isHtml5Ejecta
+		isHtml5Ejecta,
+		isHtml5GameClosure
 	) {
 		'use strict'
 
@@ -13,11 +15,17 @@ define(
 			logEvent : function( eventName, timed ) {
 				if( isHtml5Ejecta ) {
 					ejecta.flurryLogEvent( eventName, timed )
+
+				} else if( isHtml5GameClosure ) {
+					NATIVE.flurry.logEvent( eventName )
 				}
 			},
 			endTimedEvent : function( eventName ) {
 				if( isHtml5Ejecta ) {
 					ejecta.flurryEndTimedEvent( eventName )
+
+				} else if( isHtml5GameClosure ) {
+					NATIVE.flurry.endTimedEvent( eventName )
 				}
 			}
 		}
