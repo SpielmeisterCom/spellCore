b5317810e447a54709c127924ac28a6365f0542d
Martin Dederer <md@spielmeister.com>
2013-06-28 12:46:13 +0200

    Cleaning up

--- src/spell/shared/Configuration.js
+++ src/spell/shared/Configuration.js
@@ -1,18 +1,13 @@
 define(
 	'spell/shared/Configuration',
-	[
-	],
-	function(
-	) {
+	function() {
 		'use strict'
 
 
-		var Configuration = {
-		        version                : '0.8.24',
-        		buildNumber            : '99999',
-        		buildTimeStamp         : '2099-01-01T01:00:00.000+01:00',
+		return {
+			version        : '0.8.24',
+			buildNumber    : '99999',
+			buildTimeStamp : '2099-01-01T01:00:00.000+01:00'
 		}
-
-		return Configuration 
 	}
 )
