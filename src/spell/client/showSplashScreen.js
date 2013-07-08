b404f6c99977a016cc101cb30d96f04eeb44b32b
Martin Dederer <martin.dederer@spielmeister.com>
2013-07-08 19:08:44 +0200

    Fixed issue that prevented splash screen from appearing on the flash target
     * Apparently the flash rendering context is not initialized to the same level as the web rendering contexts right after creation. Adding additional initialization which used to happen only in the render system setup mitigates this issue.

--- src/spell/client/showSplashScreen.js
+++ src/spell/client/showSplashScreen.js
@@ -1,11 +1,17 @@
 define(
 	'spell/client/showSplashScreen',
 	[
+		'spell/math/mat3',
+		'spell/math/util',
 		'spell/math/vec2',
+		'spell/math/vec4',
 		'spell/shared/util/platform/PlatformKit'
 	],
 	function(
+		mat3,
+		mathUtil,
 		vec2,
+		vec4,
 		PlatformKit
 	) {
 		'use strict'
@@ -31,7 +37,18 @@ define(
 						Math.round( screenSize[ 1 ] * 0.5 - splashTexture.dimensions[ 1 ] * 0.5 )
 					)
 
-					spell.renderingContext.drawTexture( splashTexture, position, splashTexture.dimensions )
+					var context    = spell.renderingContext,
+						tmpMat3    = mat3.create(),
+						clearColor = vec4.fromValues( 0, 0, 0, 1 )
+
+					mathUtil.mat3Ortho( tmpMat3, 0.0, screenSize[ 0 ], 0.0, screenSize[ 1 ] )
+
+					context.setViewMatrix( tmpMat3 )
+					context.setClearColor( clearColor )
+					context.resizeColorBuffer( screenSize[ 0 ], screenSize[ 1 ] )
+					context.viewport( 0, 0, screenSize[ 0 ], screenSize [ 1 ] )
+
+					context.drawTexture( splashTexture, position, splashTexture.dimensions, null )
 
 					PlatformKit.registerTimer( next, 2000 )
 				}
