6ade0a1175d4b0ab029929009adc3f7a658127c6
Martin Dederer <md@spielmeister.com>
2013-08-14 16:40:14 +0200

    Cleaned up documentation of spell/math/random/XorShift32

--- src/spell/math/random/XorShift32.js
+++ src/spell/math/random/XorShift32.js
@@ -1,42 +1,35 @@
-/**
- * Xorshift is a pseudorandom number generator. It generates the next number in
- * a sequence by repeatly taking the exclusive OR of a number with a bit shifted version of itself.
- *
- * You can use it to always generate the same sequence of random numbers depending of the choosen seed.
- *
- * Example:
- *
- *     var randomNumberGenerator = new XorShift32( 12345 );
- *     //=> new Instance of Xorshift with seed 12345
- *
- *     var randomNumber1 = randomNumberGenerator.{@link #next}();
- *     var randomNumber2 = randomNumberGenerator.{@link #next}();
- *
- *     //=> always returns the same random number sequence depending on the seed
- *
- * @class spell.math.random.XorShift32
- */
-
-/**
- * Create a new instance of XorShift32 and initialize the seed with the given parameter.
- *
- * @param {Number} seed
- * @constructor
- */
 define(
 	'spell/math/random/XorShift32',
 	function() {
 		'use strict'
 
+
+		/**
+		 * XorShift32 is a pseudo random number generator. It generates the next number in a sequence by repeatly applying a sequence of bit operations. You can use it
+		 * to generate a sequence of random numbers. Since the algorithm is deterministic the generated sequence depends only on the chosen seed.
+		 *
+		 * Example:
+		 *
+		 *     // new instance of XorShift32 with seed 12345
+		 *     var prng = new XorShift32( 12345 );
+		 *
+		 *     // always returns the same pseudo random number sequence depending on the seed
+		 *     var number1 = prng.{@link #next}();
+		 *     var number2 = prng.{@link #next}();
+		 *
+		 * @class spell.math.random.XorShift32
+		 */
 		var XorShift32 = function( seed ) {
 			this.x = seed
 		}
 
-		/**
-		 * Return the next pseudorandom number in the sequence
-		 * @return {Number}
-		 */
-		XorShift32.prototype.next = function() {
+		XorShift32.prototype = {
+			/**
+			 * Returns the next number in the sequence.
+			 *
+			 * @return {Number}
+			 */
+			next : function() {
 				var a = this.x,
 					b = a
 
@@ -53,18 +46,19 @@ define(
 				this.x = b
 
 				return ( b + 2147483648 ) * ( 1 / 4294967296 )
-			}
+			},
 
-		/**
-		 * Return the next pseudorandom number between min and max in the sequence
-		 * @param {Number} min The minimum value
-		 * @param {Number} max The maximum value
-		 * @return {Number}
-		 */
-		XorShift32.prototype.nextBetween = function( min, max ) {
+			/**
+			 * Returns the next number between min and max in the sequence.
+			 *
+			 * @param {Number} min The minimum value
+			 * @param {Number} max The maximum value
+			 * @return {Number}
+			 */
+			nextBetween : function( min, max ) {
 				return ( min + this.next() * ( max - min ) )
+			}
 		}
-
 
 		return XorShift32
 	}
