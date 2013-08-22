8de1ea3ac1e9d3235b46916eb2b1a68a8a4a6361
Martin Dederer <md@spielmeister.com>
2013-08-22 18:09:01 +0200

    Exclude spell/math/random/UUID from documentation

--- src/spell/math/random/UUID.js
+++ src/spell/math/random/UUID.js
@@ -1,10 +1,11 @@
 /**
  * UUID.core.js: The minimal subset of the RFC-compliant UUID generator UUID.js.
  *
- * @fileOverview
- * @author  LiosK
- * @version core-1.0
- * @license The MIT License: Copyright (c) 2012 LiosK.
+ * author:  LiosK
+ * version: core-1.0
+ * license: The MIT License: Copyright (c) 2012 LiosK.
+ *
+ * @private
  */
 define(
 	'spell/math/random/UUID',
@@ -12,50 +13,60 @@ define(
 		'use strict'
 
 
-		/** @constructor */
-		var UUID = function() {}
-
 		/**
-		 * The simplest function to get an UUID string.
-		 * @returns {string} A version 4 UUID string.
+		 * Returns an unsigned x-bit random integer (0 <= f(x) < 2^x).
+		 *
+		 * @private
+		 * @param {Number} x A positive integer ranging from 0 to 53, inclusive.
+		 * @returns {Number}
 		 */
-		UUID.generate = function() {
-			var rand = UUID._gri, hex = UUID._ha;
-			return  hex(rand(32), 8)          // time_low
-				+ "-"
-				+ hex(rand(16), 4)          // time_mid
-				+ "-"
-				+ hex(0x4000 | rand(12), 4) // time_hi_and_version
-				+ "-"
-				+ hex(0x8000 | rand(14), 4) // clock_seq_hi_and_reserved clock_seq_low
-				+ "-"
-				+ hex(rand(48), 12);        // node
-		}
-
-		/**
-		 * Returns an unsigned x-bit random integer.
-		 * @param {int} x A positive integer ranging from 0 to 53, inclusive.
-		 * @returns {int} An unsigned x-bit random integer (0 <= f(x) < 2^x).
-		 */
-		UUID._gri = function(x) { // _getRandomInt
+		var getRandomInt = function(x) {
 			if (x <   0) return NaN;
 			if (x <= 30) return (0 | Math.random() * (1 <<      x));
 			if (x <= 53) return (0 | Math.random() * (1 <<     30))
 				+ (0 | Math.random() * (1 << x - 30)) * (1 << 30);
 			return NaN;
-		}
+		};
 
 		/**
 		 * Converts an integer to a zero-filled hexadecimal string.
-		 * @param {int} num
-		 * @param {int} length
-		 * @returns {string}
+		 *
+		 * @private
+		 * @param {Number} num
+		 * @param {Number} length
+		 * @returns {String}
 		 */
-		UUID._ha = function(num, length) {  // _hexAligner
+		var hex = function(num, length) {
 			var str = num.toString(16), i = length - str.length, z = "0";
 			for (; i > 0; i >>>= 1, z += z) { if (i & 1) { str = z + str; } }
 			return str;
-		}
+		};
+
+
+		/**
+		 * Enables the generation of UUIDs. See http://en.wikipedia.org/wiki/Universally_unique_identifier.
+		 *
+		 * @class spell.math.random.UUID
+		 * @constructor
+		 */
+		var UUID = function() {}
+
+		/**
+		 * Generates an UUID string.
+		 *
+		 * @returns {String}
+		 */
+		UUID.generate = function() {
+			return  hex(getRandomInt(32), 8)        // time_low
+				+ "-"
+				+ hex(getRandomInt(16), 4)          // time_mid
+				+ "-"
+				+ hex(0x4000 | getRandomInt(12), 4) // time_hi_and_version
+				+ "-"
+				+ hex(0x8000 | getRandomInt(14), 4) // clock_seq_hi_and_reserved clock_seq_low
+				+ "-"
+				+ hex(getRandomInt(48), 12);        // node
+		};
 
 		return UUID
 	}
