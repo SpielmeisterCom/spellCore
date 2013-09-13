41eff3c701f25fe1be40e1d01224e824bd3961fa
Martin Dederer <md@spielmeister.com>
2013-09-13 11:39:02 +0200

    Cleaning up

--- src/spell/shared/util/arrayRemove.js
+++ src/spell/shared/util/arrayRemove.js
@@ -5,14 +5,12 @@ define(
 
 
 		// Array Remove - By John Resig (MIT Licensed)
-		var arrayRemove = function( array, from, to ) {
+		return function( array, from, to ) {
 			var rest = array.slice( ( to || from ) + 1 || array.length )
 
 			array.length = from < 0 ? array.length + from : from
 
 			return array.push.apply( array, rest )
 		}
-
-		return arrayRemove
 	}
 )
