d83f0345cbab9143e26d09fbb65ed778c3e11f8e
Julian Haupt <julian.haupt@spielmeister.com>
2013-11-04 13:03:23 +0100

    Added Application.close() support for tizen

--- src/spell/shared/util/platform/private/Application.js
+++ src/spell/shared/util/platform/private/Application.js
@@ -1,10 +1,12 @@
 define(
 	'spell/shared/util/platform/private/Application',
 	[
-		'spell/shared/util/platform/private/isHtml5GameClosure'
+		'spell/shared/util/platform/private/isHtml5GameClosure',
+		'spell/shared/util/platform/private/isHtml5Tizen'
 	],
 	function(
-		isHtml5GameClosure
+		isHtml5GameClosure,
+	    isHtml5Tizen
 	) {
 		'use strict'
 
@@ -13,6 +15,9 @@ define(
 			close : function() {
 				if( isHtml5GameClosure ) {
 					NATIVE.sendActivityToBack()
+
+				} else if ( isHtml5Tizen ) {
+					tizen.application.getCurrentApplication().exit()
 				}
 			}
 		}
