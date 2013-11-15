403d2057522b5c0c10dad67443859c35d2d08fdc
Julian Haupt <julian.haupt@spielmeister.com>
2013-11-15 20:51:29 +0100

    added tizen openURL implementation

--- src/spell/shared/util/platform/private/openURL.js
+++ src/spell/shared/util/platform/private/openURL.js
@@ -2,11 +2,13 @@ define(
 	'spell/shared/util/platform/private/openURL',
 	[
 		'spell/shared/util/platform/private/isHtml5CocoonJS',
-		'spell/shared/util/platform/private/isHtml5Ejecta'
+		'spell/shared/util/platform/private/isHtml5Ejecta',
+		'spell/shared/util/platform/private/isHtml5Tizen'
 	],
 	function(
 		isHtml5CocoonJS,
-		isHtml5Ejecta
+		isHtml5Ejecta,
+	    isHtml5Tizen
 	) {
 		'use strict'
 
@@ -17,6 +19,19 @@ define(
 
 			} else if( isHtml5CocoonJS ) {
 				CocoonJS.App.openURL( url )
+
+			} else if ( isHtml5Tizen ) {
+				var appControl = new tizen.ApplicationControl(
+					'http://tizen.org/appcontrol/operation/view',
+					url
+				)
+
+				tizen.application.launchAppControl(
+					appControl,
+					null,
+					function() { },
+					function( e ) { }
+				)
 
 			} else {
 				window.open( url, message )
