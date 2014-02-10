5687f57686cbbd40658a6cee4651782a19e623de
Julian Haupt <julian.haupt@spielmeister.com>
2014-02-10 21:37:16 +0100

    added a test case for TextLoader

---
+++ test/spellTest/shared/util/platform/private/loader/TextLoader.js
@@ -0,0 +1,43 @@
+define(
+	'spellTest/shared/util/platform/private/loader/TextLoader',
+	[
+		'chai',
+		'spell/shared/util/platform/private/loader/TextLoader'
+	],
+	function(
+		chai,
+		TextLoader
+	) {
+		'use strict'
+
+		return function( describe, it ) {
+			var assert = chai.assert,
+				should = chai.should(),
+				expect = chai.expect
+
+			var textLoader = new TextLoader()
+
+			describe( 'TextLoader', function( ) {
+				it( 'should correctly load text files', function( done ) {
+					textLoader.load( 'data/test.txt', function( err, data ) {
+
+						should.not.exist( err )
+						expect( data ).to.be.equal( "Hello World\n" )
+					} )
+
+					done()
+				})
+
+				it( 'should correctly fail on non existing files', function( done ) {
+					textLoader.load( 'data/non-existent.txt', function( err, data ) {
+
+						should.exist( err )
+						should.not.exist( data )
+					} )
+
+					done()
+				})
+
+		})
+	}
+})
