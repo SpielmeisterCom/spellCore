019348536ae0dbc4baffe49635c21da070493426
Julian Haupt <julian.haupt@spielmeister.com>
2014-02-11 09:07:07 +0100

    don't rewire console.log anymore; don't register the global error handler anymore (it swallowed the original filename:linenumber information)

--- src/spell/shared/util/platform/private/initDebugEnvironment.js
+++ src/spell/shared/util/platform/private/initDebugEnvironment.js
@@ -5,31 +5,6 @@ define(
 
 
 		return function( spellConsole ) {
-			// rewiring console.log
-			console.originalLog = console.log
-
-			console.log = function() {
-				if( console.originalLog ) {
-					// trigger a warning for using console.log once and then allow console.log usage
-					console.log = console.originalLog
-					delete console.originalLog
-
-					var warning = 'console.log can be used only during development because it is not cross-platform save. Be sure to remove this call when you finished your debugging. Use spell.console to enable cross-platform logging.'
-					spellConsole.warn( warning )
-					console.warn( warning )
-
-					console.log.apply( this, arguments )
-				}
-			}
-
-			// putting global error handler in place
-			if( window ) {
-				window.onerror = function( message, url, line ) {
-					spellConsole.error( '"' + message + '" in ' + url + ':' + line )
-
-					return true
-				}
-			}
 		}
 	}
 )
