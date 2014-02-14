1f6faf0651bb092635e682b6533337003db649d3
Julian Haupt <julian.haupt@spielmeister.com>
2014-02-14 15:35:54 +0100

    added a test for BoxTree

---
+++ test/spellTest/data/spatial/BoxTree.js
@@ -0,0 +1,41 @@
+define(
+	'spellTest/data/spatial/BoxTree',
+	[
+		'chai',
+		'spell/data/spatial/BoxTree'
+	],
+	function(
+		chai,
+		BoxTree
+	) {
+		'use strict'
+
+		return function( describe, it ) {
+			var expect = chai.expect
+
+			var boxtree = BoxTree.create( true )
+
+			describe( 'BoxTree', function( ) {
+				it( 'should correctly return an added object', function( done ) {
+
+					var result = []
+					boxtree.add( {objectId:1}, [0, 0, 10, 10])
+					boxtree.add( {objectId:2}, [11, 11, 20, 20])
+
+					boxtree.finalize()
+
+
+					boxtree.getOverlappingNodes(
+						[0, 0, 10, 10],
+						result
+					)
+
+					expect( result ).to.have.length( 1 )
+					expect( result[0] ).to.have.property( 'objectId', 1 )
+
+					done()
+				})
+			})
+
+	}
+})
