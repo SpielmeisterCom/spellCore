cd05921541f92cd1d1fc408d23474741ef404fa1
Julian Haupt <julian.haupt@spielmeister.com>
2014-02-14 19:23:01 +0100

    make BoxTree & BoxTreeNode pass the mxmlc compiler

--- src/spell/data/spatial/BoxTreeNode.js
+++ src/spell/data/spatial/BoxTreeNode.js
@@ -4,45 +4,41 @@ define(
 		'use strict'
 		// Copyright (c) 2012 Turbulenz Limited
 
-		var BoxTreeNode = (function () {
-			function BoxTreeNode(extents, escapeNodeOffset, externalNode) {
-				this.escapeNodeOffset = escapeNodeOffset;
-				this.externalNode = externalNode;
-				this.extents = extents;
-			}
-			BoxTreeNode.prototype.isLeaf = function () {
-				return !!this.externalNode;
-			};
+		var BoxTreeNode = function BoxTreeNode(extents, escapeNodeOffset, externalNode) {
+			this.escapeNodeOffset = escapeNodeOffset;
+			this.externalNode = externalNode;
+			this.extents = extents;
+		}
+		BoxTreeNode.prototype.isLeaf = function () {
+			return !!this.externalNode;
+		};
 
-			BoxTreeNode.prototype.reset = function (minX, minY, maxX, maxY, escapeNodeOffset, externalNode) {
-				this.escapeNodeOffset = escapeNodeOffset;
-				this.externalNode = externalNode;
-				var oldExtents = this.extents;
-				oldExtents[0] = minX;
-				oldExtents[1] = minY;
-				oldExtents[2] = maxX;
-				oldExtents[3] = maxY;
-			};
+		BoxTreeNode.prototype.reset = function (minX, minY, maxX, maxY, escapeNodeOffset, externalNode) {
+			this.escapeNodeOffset = escapeNodeOffset;
+			this.externalNode = externalNode;
+			var oldExtents = this.extents;
+			oldExtents[0] = minX;
+			oldExtents[1] = minY;
+			oldExtents[2] = maxX;
+			oldExtents[3] = maxY;
+		};
 
-			BoxTreeNode.prototype.clear = function () {
-				this.escapeNodeOffset = 1;
-				this.externalNode = undefined;
-				var oldExtents = this.extents;
-				var maxNumber = Number.MAX_VALUE;
-				oldExtents[0] = maxNumber;
-				oldExtents[1] = maxNumber;
-				oldExtents[2] = -maxNumber;
-				oldExtents[3] = -maxNumber;
-			};
+		BoxTreeNode.prototype.clear = function () {
+			this.escapeNodeOffset = 1;
+			this.externalNode = undefined;
+			var oldExtents = this.extents;
+			var maxNumber = Number.MAX_VALUE;
+			oldExtents[0] = maxNumber;
+			oldExtents[1] = maxNumber;
+			oldExtents[2] = -maxNumber;
+			oldExtents[3] = -maxNumber;
+		};
 
-			// Constructor function
-			BoxTreeNode.create = function (extents, escapeNodeOffset, externalNode) {
-				return new BoxTreeNode(extents, escapeNodeOffset, externalNode);
-			};
-			BoxTreeNode.version = 1;
-			return BoxTreeNode;
-		})();
-
+		// Constructor function
+		BoxTreeNode.create = function (extents, escapeNodeOffset, externalNode) {
+			return new BoxTreeNode(extents, escapeNodeOffset, externalNode);
+		};
+		BoxTreeNode.version = 1;
 		return BoxTreeNode;
 	}
 )
