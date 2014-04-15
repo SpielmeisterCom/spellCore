08aadae1ba075d94ffd35be9a0a6752943e01eb3
Ioannis Kostomanolakis <ik@spielmeister.com>
2014-04-15 17:48:02 +0200

    First version of android iap

--- src/spell/shared/util/platform/private/iap/android.js
+++ src/spell/shared/util/platform/private/iap/android.js
@@ -1,9 +1,6 @@
 define(
 	'spell/shared/util/platform/private/iap/android',
-	[
-	],
-	function(
-		) {
+	function() {
 		'use strict'
 
 
@@ -16,6 +13,51 @@ define(
             notPurchased: 3
         }
 
+        var onFailure
+        var onPurchase
+
+        NATIVE.events.registerHandler('billingPurchase', function( evt ) {
+            console.log( "Got billingPurchase event:", JSON.stringify( evt ) )
+
+            // If SKU event,
+            var sku   = evt.sku,
+                token = evt.token
+
+            if (!sku || evt.failure) {
+                var failure = evt.failure || "cancel"
+
+                console.log( "Unable to purchase item", sku, ":", failure )
+
+                if (typeof onFailure === "function") {
+                    onFailure( failure, sku )
+                }
+            } else {
+                NATIVE.plugins.sendEvent( "BillingPlugin", "consume", JSON.stringify({
+                    token: token
+                }))
+            }
+        })
+
+        NATIVE.events.registerHandler( 'billingConsume', function( evt ) {
+            console.log( "Got billingConsume event:", JSON.stringify( evt ) )
+
+            // NOTE: Function is organized carefully for callback reentrancy
+
+            var token = evt.token
+
+            // If not failed,
+            if ( !evt.failure ) {
+                //Consume item
+                console.log( "Consumed token", token )
+
+                if ( typeof onPurchase === "function" ) {
+                    onPurchase()
+                }
+            } else {
+                console.log( "Failed to consume token", token )
+            }
+        })
+
 		return {
 			init: function( isDebug ) {
 				simulate = isDebug
@@ -24,10 +66,11 @@ define(
 				return false
             },
             purchaseProduct: function( productId, successCallback, errorCallback ) {
-				//TODO: look at https://github.com/gameclosure/billing/blob/master/js/billing.js
-				NATIVE.plugins.sendEvent("BillingPlugin", "purchase", JSON.stringify({
-					"sku": productId
-				}))
+                onFailure = errorCallback
+                onPurchase = successCallback
+                NATIVE.plugins.sendEvent( "BillingPlugin", "purchase", JSON.stringify({
+                    "sku": productId
+                }))
             }
 		}
 	}
