f80af3be29c72fa26735a8fb4ed1f13ea91494d6
Julian Haupt <julian.haupt@spielmeister.com>
2014-05-08 01:01:00 +0200

    added getAssetIdReferencesForEntities Helper function with test

---
+++ test/spellTest/library/getAssetIdReferencesForEntities.js
@@ -0,0 +1,59 @@
+define(
+	[
+		'spell/library/getAssetIdReferencesForEntities'
+	],
+	function(
+		getAssetIdReferencesForEntities
+	) {
+		'use strict'
+
+			var testLibrary = {
+				"test.Component": {
+					"type": "component",
+					"attributes": [
+						{
+							"name": "assetId",
+							"type": "assetId:spriteSheet",
+							"default": "appearance:spell.defaultAppearance",
+							"doc": "the spritesheet asset used for rendering"
+						}
+					]
+				}
+			}
+
+			it( 'should correctly resolve referenced assetIds', function( done ) {
+
+				var referencedAssetIds = getAssetIdReferencesForEntities(
+					testLibrary,
+					[{
+						'name': 'test',
+						config: {
+							'test.OtherComponent': {
+								'assetId': 'apperance:test'
+							},
+
+							'test.Component': {
+								'assetId': 'apperance:test'
+							}
+						},
+
+						children: [{
+							'name': 'child',
+							'config': {
+								'test.Component': {
+									'assetId': 'apperance:test2'
+								}
+							}
+						}]
+					}]
+				)
+
+				expect( referencedAssetIds ).to.have.length( 2 )
+				expect( referencedAssetIds[ 0 ] ).to.be.equal( 'apperance:test' )
+				expect( referencedAssetIds[ 1 ] ).to.be.equal( 'apperance:test2' )
+
+
+				done()
+			} )
+
+})
